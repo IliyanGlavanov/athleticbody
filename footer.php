541c4f89d310618813016b89e2c18f6f8ea1d237
Iliyan Glavanov <iliqn.glavanov@abv.bg>
2018-03-25 11:35:48 +0300

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,12 +1,1 @@
-<footer class="container">
-  <div class="row footer-social-icons">
-    <h4 class="_14">Follow us on</h4>
-    <ul class="social-icons">
-      <li><a href="" class="social-icon" data-toggle="tooltip" title="Facebook"> <i class="fa fa-facebook"></i></a></li>
-      <li><a href="" class="social-icon" data-toggle="tooltip" title="Twitter"> <i class="fa fa-twitter"></i></a></li>
-      <li><a href="" class="social-icon" data-toggle="tooltip" title="Instagram"> <i class="fa fa-instagram"></i></a></li>
-      <li><a href="" class="social-icon" data-toggle="tooltip" title="YouTube"> <i class="fa fa-youtube"></i></a></li>
-      <li><a href="" class="social-icon" data-toggle="tooltip" title="Google"> <i class="fa fa-google-plus"></i></a></li>
-    </ul>
-  </div>
-</footer>
+<footer> <div class=" footer-social-icons"> <h4 class="followus">Последвай ни</h4> <ul class="social-icons"> <li><a href="https://www.facebook.com/athleticbodyGM" class="social-icon" data-toggle="tooltip" title="Facebook"> <i class="fa fa-facebook"></i></a></li><li><a href="" class="social-icon" data-toggle="tooltip" title="Twitter"> <i class="fa fa-twitter"></i></a></li><li><a href="" class="social-icon" data-toggle="tooltip" title="Instagram"> <i class="fa fa-instagram"></i></a></li><li><a href="https://www.youtube.com/channel/UC5hwhs4Iz0zqT1Am3KWrY3Q" class="social-icon" data-toggle="tooltip" title="YouTube"> <i class="fa fa-youtube"></i></a></li><li><a href="" class="social-icon" data-toggle="tooltip" title="Google"> <i class="fa fa-google-plus"></i></a></li></ul> </div></footer>
--- index.php
+++ index.php
@@ -4,21 +4,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <!-- Google fonts "Bad script" -->
-  <link href="https://fonts.googleapis.com/css?family=Bad+Script" rel="stylesheet"/>
   <!-- My custom .CSS -->
-  <link type="text/css" rel="stylesheet" href="../mainstyle.css?=40"/>
+  <link type="text/css" rel="stylesheet" href="../mainstyle.css" onload="if(media!='all')media='all'"/>
 </head>
 
 <body>
-  <div class="container" id="backquote">
-    <blockquote class="col-lg-12 col-md-12">
+  <script type="text/javascript">
+      $(window).on('load',function(){
+          $('#myModal').modal('show');
+      });
+  </script>
+  <!-- Modal -->
+  <div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-md">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="text-center">
+        <div class="fb-page" data-href="https://www.facebook.com/athleticbodyGM/" data-width="280" data-height="600" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/athleticbodyGM/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/athleticbodyGM/">Athleticbody / Motivation and advices</a></blockquote></div></div>
+          <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container text-center" id="backquote">
+    <blockquote class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
       <p>Не можете да върнете времето и да промените вашия старт, но можете да започнете сега и да промените финала си! Моментната и бъдещата мотивация са безкомпромисния избор към успеха. Бори се до край, болката е временна, резултата е вечен!</p>
     </blockquote>
   </div>
   <div class="container">
     <div class="row">
-      <section class="block">
+      <section class="block" >
         <div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
           <!-- Carousel indicators -->
           <ol class="carousel-indicators">
@@ -29,13 +48,13 @@
           <!-- Carousel items -->
           <div class="carousel-inner">
             <div class="item active">
-              <img src="slideshow1.jpg" alt="First Slide">
+              <img src="slideshow12.jpg" alt="First Slide">
             </div>
             <div class="item">
-              <img src="slideshow3.jpg" alt="First Slide">
+              <img src="slideshow33.jpg" alt="First Slide">
             </div>
             <div class="item">
-              <img src="slideshow4.jpg" alt="Third Slide">
+              <img src="slideshow44.jpg" alt="Third Slide">
             </div>
           </div>
           <!-- Carousel nav -->
@@ -52,45 +71,46 @@
 <!-- HERE STARTS THE *ARTICLE* CARD -->
   <div class="index-content">
     <div class="container">
-      <a href="blog-ici.html">
-        <div class="col-lg-4">
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
           <div class="card">
-            <a href="../statii/stranica1/statiq6/statiq6.php">
+            <a href="../statii/stranica1/statiq6/statiq6">
               <img src="article1.jpg">
                 <h4>Проект „Мускулна маса” -  за кого е предназначен той и какво представлява?</h4>
                 <p>Това е тренировъчен сплит съставен изцяло от базови (многоставни) упражнения, подходящ за хора, които искат да направят т.н. „основен период”, т.е. искат да покачат килограми, да станат по обемни и огромни.</p>
-                <a href="../statii/stranica1/statiq6/statiq6.php" class="blue-button">Прочети</a>
+                <a href="../statii/stranica1/statiq6/statiq6" class="blue-button">Прочети</a>
             </a>
           </div>
         </div>
-      </a>
-      <a href="blog-ici.html">
-        <div class="col-lg-4">
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
           <div class="card">
-          <a href="../statii/stranica1/statiq1/statiq1.php">
+          <a href="../statii/stranica1/statiq1/statiq1">
               <img src="statiq2.jpg">
               <h4>Бързото отслабване е равно на мускулен разпад.</h4>
               <p>Как да избегнем загубата на мускулната маса и дали бързото отслабване е решение. </p>
-              <a href="../statii/stranica1/statiq1/statiq1.php" class="blue-button">Прочети</a>
+              <a href="../statii/stranica1/statiq1/statiq1" class="blue-button">Прочети</a>
           </a>
           </div>
         </div>
-      </a>
-      <a href="blog-ici.html">
-        <div class="col-lg-4">
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
           <div class="card">
-          <a href="../statii/stranica1/statiq2/statiq2.php">
+          <a href="../statii/stranica1/statiq2/statiq2">
               <img src="statiq3.jpg">
               <h4>За колко време идват резултатите?</h4>
               <p>Искате бързи резултати, нямате търпение да станете огромни и релефни? Разберете реалността и оптималното време, за което ще постигнете всичко това.</p>
-              <a href="../statii/stranica1/statiq2/statiq2.php" class="blue-button">Прочети</a>
+              <a href="../statii/stranica1/statiq2/statiq2" class="blue-button">Прочети</a>
           </a>
           </div>
         </div>
-      </a>
-
     </div>
   </div>
+  <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/bg_BG/sdk.js#xfbml=1&version=v2.12&appId=1675475726080991&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <!-- HERE ENDS THE *ARTICLE* CARD -->
 <div id="contactus">
   <?php
